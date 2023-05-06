6e3e88e7ed16f9f41064bf4476fe635c8b17b266
macbook <macbook@macbooks-MacBook-Air.local>
2023-05-06 07:27:58 +0530

    final commit

--- server.js
+++ server.js
@@ -5,5 +5,7 @@ const port = process.env.PORT;
 
 //listening to the server tha will be running on the port 80
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(
+    `Server running at http://localhost:${port} in ${process.env.NODE_ENV} mode`
+  );
 });
